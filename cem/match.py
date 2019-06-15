372b6d5608a7e2650237a34bc8f630b659784468
lmc2179 <louiscialdella@gmail.com>
2019-06-15 16:47:21 -0500

    added stratum and matching class structure

--- cem/match.py
+++ cem/match.py
@@ -1,2 +1,18 @@
 import numpy as np
 import pandas as pd
+
+class Stratum(object):
+    def __init__(self, signature, y_control, y_treatment):
+        self.signature = signature
+        self.y_control = y_control
+        self.y_treatment = y_treatment
+        self.difference = np.mean(y_treatment) - np.mean(y_control)
+        self.sigma = np.sqrt((np.var(y_control)/len(y_control)) + (np.var(y_treatment)/len(y_treatment)))
+
+    def get_difference(self):
+        return self.difference
+
+class ExactMatching(object):
+    def match(self, X, t, y):
+        """Perform exact matching using covariates X, treatment indicator t, and response variable y."""
+        pass
--- cem/tests/test_match.py
+++ cem/tests/test_match.py
@@ -1,4 +1,14 @@
 import unittest
 import numpy as np
 import random
-import cem
+from cem.match import ExactMatching, Stratum
+
+class StratumTest(unittest.TestCase):
+    def test_stratum(self):
+        s = Stratum(0, [0,1,2], [1,2,3])
+        delta = s.get_difference()
+        self.assertEqual(delta, 1)
+
+class MatchTest(unittest.TestCase):
+    def test_match(self):
+        m = ExactMatching()
